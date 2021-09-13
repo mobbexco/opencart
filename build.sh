c8ff97f2c6f5ccfc8ee18ac9312a4498b9b54c20
jlmobbex <javier.lopez@mobbex.com>
2021-09-13 12:12:11 -0300

    Support opencart wizard extension installer

--- build.sh
+++ build.sh
@@ -2,8 +2,15 @@
 
 VER="1.0.0"
 
+# Create upload temporal directory
+mkdir upload && cp -r admin catalog system ./upload
+
+# Compress files
 if type 7z > /dev/null; then
-    7z a -tzip "mobbex.$VER.oc.zip" admin catalog system
+    7z a -tzip "mobbex.$VER.ocmod.zip" upload
 elif type zip > /dev/null; then
-    zip mobbex.$VER.oc.zip -r admin catalog system
+    zip mobbex.$VER.ocmod.zip -r upload
 fi
+
+# Remove upload directory
+rm -r upload
