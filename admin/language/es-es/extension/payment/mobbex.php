91b6b6c87322990fa70c087815719d719f3f7864
mobbexLuis <luis.scarrone@mobbex.com>
2023-04-24 15:22:48 -0300

    new feat : debug mode

--- admin/language/es-es/extension/payment/mobbex.php
+++ admin/language/es-es/extension/payment/mobbex.php
@@ -2,17 +2,18 @@
 
 $_ = [
     // Extension list and plugin config page translations
-    'config_title'     => 'Configuración de Mobbex',
-    'heading_title'    => 'Mobbex',
-    'text_mobbex'      => '<a href="https://mobbex.com" target="_blank"><img src="view/image/mobbex.png" alt="Mobbex" title="Mobbex" width="100px"/></a>',
-    'success_save'     => 'Configuración modificada correctamente.',
-    'error_permission' => 'Warning: You do not have permission to modify this config!',
+    'config_title'      => 'Configuración de Mobbex',
+    'heading_title'     => 'Mobbex',
+    'text_mobbex'       => '<a href="https://mobbex.com" target="_blank"><img src="view/image/mobbex.png" alt="Mobbex" title="Mobbex" width="100px"/></a>',
+    'success_save'      => 'Configuración modificada correctamente.',
+    'error_permission'  => 'Warning: You do not have permission to modify this config!',
 
-    'text_enabled'     => 'Activado',
-    'text_disabled'    => 'Desactivado',
+    'text_enabled'      => 'Activado',
+    'text_disabled'     => 'Desactivado',
 
-    'status'           => 'Estado',
-    'api_key'          => 'Clave de API',
-    'access_token'     => 'Token de Acceso',
-    'test_mode'        => 'Modo de Pruebas',
+    'status'            => 'Estado',
+    'api_key'           => 'Clave de API',
+    'access_token'      => 'Token de Acceso',
+    'test_mode'         => 'Modo de Pruebas',
+    'debug_mode'        => 'Modo Depuración',
 ];
