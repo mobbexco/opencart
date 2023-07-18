9e92c12e3e777c2d7b63a2a3fe72ee133b37ae85
Emiliano Muñoz Chedel <118913149+EMunozMobbex@users.noreply.github.com>
2023-07-18 13:02:39 -0300

    Fix: Changes requested by @mobbexLuis in #13

--- admin/language/es-es/extension/payment/mobbex.php
+++ admin/language/es-es/extension/payment/mobbex.php
@@ -6,7 +6,7 @@ $_ = [
     'heading_title'     => 'Mobbex',
     'text_mobbex'       => '<a href="https://mobbex.com" target="_blank"><img src="view/image/mobbex.png" alt="Mobbex" title="Mobbex" width="100px"/></a>',
     'success_save'      => 'Configuración modificada correctamente.',
-    'error_permission'  => 'Warning: You do not have permission to modify this config!',
+    'error_permission'  => '¡No tienes permisos para modificar esta configuración!',
 
     'text_enabled'      => 'Activado',
     'text_disabled'     => 'Desactivado',
