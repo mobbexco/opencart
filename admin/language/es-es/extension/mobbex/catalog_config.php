45e0ffb07857028352f36d118dc1f09d3e2c903f
EMunozMobbex <e.munoz.chedel@mobbex.com>
2023-09-22 11:52:47 -0300

    feat: Add translations.

--- admin/language/es-es/extension/mobbex/catalog_config.php
+++ admin/language/es-es/extension/mobbex/catalog_config.php
@@ -5,4 +5,7 @@ $_ = [
     'plans_config_title'   => 'Configuración de Planes',
     'common_plans_label'   => 'Planes Comunes',
     'advanced_plans_label' => 'Planes con Reglas Avanzadas',
+    'multivendor_title'    => 'Multivendor',
+    'multivendor_label'    => 'UID del Vendedor',
+    'multivendor_dsec'     => 'Coloca el UID del vendedor asignado a este producto.',
 ];
